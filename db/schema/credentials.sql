9f66b9de63e434be8b9cdc979bd7cc1fc586ce70
Clayton Durepos <145684590+CDurepos@users.noreply.github.com>
2025-11-13 22:15:24 -0500

    updated grants->org join table name, updates to credentials property names

--- db/schema/credentials.sql
+++ db/schema/credentials.sql
@@ -1,9 +1,9 @@
 CREATE TABLE credentials (
-    faculty_id  CHAR(36) NOT NULL,
-    username    VARCHAR(255) NOT NULL UNIQUE,
-    pw_hash     VARCHAR(255) NOT NULL,
-    pw_salt     VARCHAR(255) NOT NULL,
-    last_login  DATETIME NULL,
+    faculty_id        CHAR(36) NOT NULL,
+    username          VARCHAR(255) NOT NULL UNIQUE,
+    password_hash     VARCHAR(255) NOT NULL,
+    password_salt     VARCHAR(255) NOT NULL,
+    last_login        DATETIME NULL,
 
     PRIMARY KEY (faculty_id),
 
---
+++ db/schema/grants_provided_by_organization.sql
@@ -0,0 +1,10 @@
+CREATE TABLE grants_provided_by_organization (
+    grant_id       CHAR(36) NOT NULL,
+    name           VARCHAR(256) NOT NULL,
+    PRIMARY KEY (grant_id),
+
+    FOREIGN KEY (grant_id)
+        REFERENCES grants (grant_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
