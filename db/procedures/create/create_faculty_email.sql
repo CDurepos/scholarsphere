c221f90b796ee325760e25a7e3bab86aaba4e525
Clayton Durepos <145684590+CDurepos@users.noreply.github.com>
2025-11-16 00:09:43 -0500

    updated  naming to

--- db/procedures/create/create_faculty_email.sql
+++ db/procedures/create/create_faculty_email.sql
@@ -10,7 +10,7 @@ DELIMITER $$
  * @param p_faculty_id  Required UUID of the faculty member
  * @param p_email       Required email address to associate with the faculty member
  * 
- * @returns No result set (unlike other insert procedures)
+ * @returns No result set (unlike other create procedures)
  */
 CREATE PROCEDURE insert_faculty_email (
     IN p_faculty_id CHAR(36),
@@ -20,7 +20,7 @@ BEGIN
     -- Insert a new email address for the faculty member
     -- Faculty can have multiple email addresses (e.g., work, personal, lab)
     -- The combination of faculty_id and email should be unique
-    -- Note: This procedure does not return a result set (unlike other insert procedures)
+    -- Note: This procedure does not return a result set (unlike other create procedures)
     INSERT INTO faculty_email (faculty_id, email)
     VALUES (p_faculty_id, p_email);
 END$$
